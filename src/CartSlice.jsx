21bf55e08b4597cf373011721f3baea54b49dee6
Imran Ansari <azhararman5009@gmail.com>
2025-03-16 11:57:22 +0530

    Final commit - Pushing to remote repository

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -8,7 +8,7 @@ export const CartSlice = createSlice({
   reducers: {
     addItem: (state, action) => {
       const {name, image, cost } = action.payload;
-      const existingItem = state.items.find(item.name === name);
+      const existingItem = state.items.find(item => item.name === name);
       if (existingItem) {
         existingItem.quantity++;
       } else {
@@ -16,7 +16,7 @@ export const CartSlice = createSlice({
       }
     },
     removeItem: (state, action) => {
-      state.items = state.items.filter(item.name !== action.payload);
+      state.items = state.items.filter(item => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
       const { name, quantity } = action.payload;
